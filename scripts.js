6d6e1894e12bfb12694008f8203cef642af0a2be
riannevg <85849537+riannevg@users.noreply.github.com>
2021-09-19 20:59:13 +0200

    page

---
+++ scripts.js
@@ -0,0 +1,14 @@
+$('.slider-container').slick({
+	autoplay: true, // Do we want it to autoplay? true or false
+	autoplaySpeed: 3000, // How long between each slide when auto-playing
+	speed: 500, // How fast is the transition in milliseconds
+	arrows: true, // Do you want to show arrows to trigger each slide
+	accessibility: true, // Enables keyboard tabbing and arrow key navigation
+	dots: true, // Enables the dots below to show how many slides
+	fade: true, // Changes the animate from slide to fade if true
+	infinite: true, // When true, means that it will scroll in a circle
+	pauseOnHover: true, // When true means the autoplay pauses when hovering
+	pauseOnDotsHover: true // Pauses the autoplay when hovering over the dots
+});
+
+
